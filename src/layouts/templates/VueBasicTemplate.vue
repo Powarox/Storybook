73649aa1b6d4dd4a2288c854bbf9151fd70dda30
Powarox2159 <moriniere.robin1877@gmail.com>
2022-04-17 18:19:33 +0200

    feat: create compoenent MenuPopUp

--- src/layouts/templates/VueBasicTemplate.vue
+++ src/layouts/templates/VueBasicTemplate.vue
@@ -1,13 +1,17 @@
 <template lang="html">
-  
+    <div id="component">
+        
+    </div>
 </template>
 
 <script>
     export default {
-
+        name: "component",
     }
 </script>
 
 <style lang="css" scoped>
-
+    #component {
+        font-family: sans-serif;
+    }
 </style>
