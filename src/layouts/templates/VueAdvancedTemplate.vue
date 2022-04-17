72dd165d5ddf49a4e9ec5fd65c597b7551803c0d
Powarox2159 <moriniere.robin1877@gmail.com>
2022-04-17 18:06:17 +0200

    feat: create vuejs code templates

---
+++ src/layouts/templates/VueAdvancedTemplate.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+    <div id="VueAdvencedTemplate">
+        <Components
+            :id="id"
+        />
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import components from "./components.vue";
+
+export default {
+    name: "VueAdvencedTemplate",
+    props: [],
+    components: {
+        components,
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        ...mapActions([]),
+        ...mapGetters([]),
+    },
+    computed: {},
+};
+</script>
+
+<style lang="css" scoped></style>
